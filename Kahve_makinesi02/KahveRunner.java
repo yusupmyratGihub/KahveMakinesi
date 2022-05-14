21adda8ca18669c4829f29c8b3ba34dc28abf156
Enes Faruk Baltacı <efarukbaltaci@gmail.com>
2022-05-14 15:24:27 +0200

    by Enes

--- Kahve_makinesi02/KahveRunner.java
+++
@@ -1,8 +0,0 @@
-package JavaProjectsFaDu.P06_KahveMakinesi.Kahve_makinesi02;
-
-public class KahveRunner {
-    public static void main(String[] args) {
-        HangiKahve khv =new HangiKahve();
-        khv.startKahve();
-    }
-}
